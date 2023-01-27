e0fa542b53d8969a87ceec8b9a90638889487899
Charles-Desk-Xfce <charlesvill@g.ucla.edu>
2023-01-27 06:45:57 -0800

    created random number generator and started with computer choice script

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('hello world!');
+function getComputersChoice(){
+    const ranNum = Math.floor(Math.random()*3) +1;
+    const rock = 'rock';
+    const paper= 'paper';
+    const scissors= 'scissors';
+
+    switch(ranNum){
+        case '1':
+            return rock;
+            break;
+        case '2':
+            return paper;
+            break;
+        case '3':
+            return scissors;
+            break;
+        default: null;
+    }
+
+}
+
+alert(getComputersChoice());
