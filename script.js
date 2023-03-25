5be03b140c133ec43017c0e0acb26a5dfc90a10d
Bill-Bo-Baggins-Pop-OS <charlesvill@g.ucla.edu>
2023-03-25 15:23:06 -0700

    added buttons for rps, div each for results and score, round now plays with button function call

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorBtn = document.querySelector('#scissors');
+const resultsDiv = document.querySelector('#results');
+const cScoreDiv = document.querySelector('#c_score');
+const pScoreDiv = document.querySelector('#player_score');
+
+rockBtn.addEventListener('click', () => game("rock")); 
+paperBtn.addEventListener('click', () => game("paper"));
+scissorBtn.addEventListener('click', () => game("scissors"));
+
+
+
 function ranNum (){
     let number = Math.floor(Math.random()*3 +1);
     return number;
 } 
 
- function playerChoice () {
-    let choice = prompt('rock, paper, or scissors?').toLocaleLowerCase();
-    return choice; 
- } 
+ 
 
 
 function computerChoice(ranNum){
@@ -31,8 +41,9 @@ return comChoice;
 }
 
 //this function requires the passthrough of the computer choice and the input function variables
-function playRound(playerChoice, computerChoice){
+function playRound(playerSelection, computerChoice){
 
+    console.log('the function is playing!');
 const draw = 'it\'s a draw!';
 const playerWin = 'Player wins round';
 const compWin = 'Player loses round';
@@ -41,40 +52,42 @@ const error = 'there was an error, try again';
 let playerScore= 0;
 let compScore = 0;
 
+
+
 //player selects rock
-if(playerChoice==='rock'&&computerChoice==='paper'){
+if(playerSelection==='rock'&&computerChoice==='paper'){
     compScore++;
     return compWin;
 }
-else if(playerChoice==='rock'&&computerChoice==='rock'){
+else if(playerSelection==='rock'&&computerChoice==='rock'){
     return draw;
 }
-else if(playerChoice==='rock'&&computerChoice==='scissors'){
+else if(playerSelection==='rock'&&computerChoice==='scissors'){
     playerScore++;
     return playerWin;
 }
 //player selects paper
-else if(playerChoice==='paper'&&computerChoice==='paper'){
+else if(playerSelection==='paper'&&computerChoice==='paper'){
     return draw;
 }
-else if(playerChoice==='paper'&&computerChoice==='rock'){
+else if(playerSelection==='paper'&&computerChoice==='rock'){
     playerScore++;
     return playerWin;
 }
-else if(playerChoice==='paper'&&computerChoice==='scissors'){
+else if(playerSelection==='paper'&&computerChoice==='scissors'){
     compScore++;
     return compWin;
 }
 //player selects scissors
-else if(playerChoice==='scissors'&&computerChoice==='paper'){
+else if(playerSelection==='scissors'&&computerChoice==='paper'){
     playerScore++;
     return playerWin;
 }
-else if(playerChoice==='scissors'&&computerChoice==='rock'){
+else if(playerSelection==='scissors'&&computerChoice==='rock'){
     compScore++;
     return compWin;
 }
-else if(playerChoice==='scissors'&&computerChoice==='scissors'){
+else if(playerSelection==='scissors'&&computerChoice==='scissors'){
     return draw;
 }
 else{ 
@@ -85,7 +98,9 @@ else{
 
 
 
-function game(){
+function game(playerSelection)
+{
+    
     let userScore=0;
     let compScore=0;
     let rounds;
@@ -94,53 +109,29 @@ function game(){
     const draw = 'it\'s a draw!';
 
     //play 5 rounds 
-    for ( let i=0; i<5; i++){
+    //This below is being refactored with the buttons instead
+
+    
         
         //set i to round variable to pass outer scope and add one if it's on the last one
         //this is because on round5 it will exit loop before assigning i as 5.
-        if(i>=4){rounds=i+1;}
-        else{rounds=i;}
-        let playerSelection = playerChoice();
-        let computerSelection = computerChoice(ranNum());
-        let roundResults = playRound(playerSelection, computerSelection);
+        //if(i>=4){rounds=i+1;}
+        //else{rounds=i;}
+
+
+        //where i left off: the round is playing with the buttons but it does not update the score of the winner, but is displaying the results of the round.
+       
+        var computerSelection = computerChoice(ranNum());
+        var roundResults = playRound(playerSelection, computerSelection);
+        resultsDiv.textContent = `Results: ${roundResults}`;
+        cScoreDiv.textContent = `Computer Score: ${compScore}`;
+        pScoreDiv.textContent = ` Player Score: ${userScore}`;
+
         //will need to add the variable function assignments to the loop so it actually updates the values. 
-        //evaluate winner and add points
-        if(roundResults === playerWin ){
-            console.log( `${playerSelection} beats ${computerSelection} , you have won a round`);
-            userScore++;
-            if (userScore >= 3 ){
-                //endgame function where the user wins 
-                
-                i=5;
-            }
-        }
-        else if(roundResults === compWin){
-            console.log(`${computerSelection} beats ${playerSelection} , you have lost this round.`);
-            compScore++;{
-            if(compScore >=3 ){
-                //endgame function where computer wins
-                
-                i=5; 
-               }
-            }
-             //keep score and end game when 5 rounds up or one has 3 points
-        }
-        else if (roundResults === draw){
-            console.log(`between ${computerSelection} and ${playerSelection} is a draw!`)
-        }
-        
-//bug when it gets to round 5, it immediately kicks it out of the loop and does not evaluate the final score to determine a winner. 
-    }
+    }          //evaluate winner and add points
+  
 
-    if(compScore>userScore){
-        alert(`The computer has won! ${userScore} to ${compScore}, total rounds: ${rounds}`);
-    }
-    else if(compScore===userScore){
-        alert(`Looks like it was a draw! ${userScore} to ${compScore}, total rounds: ${rounds}`)
-    }
-    else{
-        alert(`you have won! ${userScore} to ${compScore}, total rounds: ${rounds}`);
-    }
-}
+    
+  
 
-game();
+
